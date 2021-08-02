e95ced1d5a3f92d51b7e1a4adfd468ae3d944d01
lavanya-pg <lavanyapg8@gmail.com>
2021-08-02 15:43:18 +0530

    Add a new contact in the addressbook

--- lib/src/main/java/addressbook/AddressBookMain.java
+++ lib/src/main/java/addressbook/AddressBookMain.java
@@ -1,11 +1,38 @@
 package addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain 
 {
+	Contact user;
+	private void addContact() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the FirstName: ");
+		String firstname = scanner.nextLine();
+		System.out.println("Enter the LastName: ");
+		String lastname = scanner.nextLine();
+		System.out.println("Enter the Address: ");
+		String address = scanner.nextLine();
+		System.out.println("Enter the City: ");
+		String city = scanner.nextLine();
+		System.out.println("Enter the State: ");
+		String state = scanner.nextLine();
+		System.out.println("Enter the PhoneNumber: ");
+		String phonenumber = scanner.nextLine();
+		System.out.println("Enter the EmailId: ");
+		String emailid = scanner.nextLine();
+		System.out.println("Enter the ZipCode: ");
+		long zipcode = scanner.nextLong();
+		user = new Contact();
+		System.out.println("Contact Added Successfully.");
+		
+	}
 	public static void main(String[] args)
 	{
 	   System.out.println("welcome to AddressBook Program!");
 	   Contact user = new Contact();
+	   AddressBookMain addressBook = new AddressBookMain();
+       addressBook.addContact();
        System.out.println(user);
 	}
 }
